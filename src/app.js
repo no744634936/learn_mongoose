16c822b05440c2909b9d0d2489982656a7a75f02
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-06-28 09:53:57 +0900

    good

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const Koa = require('koa')
+const app = new Koa()
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const render = require('koa-art-template')
+const index = require('./routes/index')
+const path=require('path')
+
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+render(app, {
+    root: path.join(__dirname, 'views'),   //视图的位置
+    extname: '.html', //后缀名
+    debug: process.env.NODE_ENV !== 'production'  //是否开启调试模式
+});
+
+// logger 演示的代码，可以删除
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
